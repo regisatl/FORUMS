9d47b17f58c498f7d819850f08f850f3f315f2ee
regisatl <regisattolou21@outlook.fr>
2023-08-29 17:50:26 +0100

    FILE HIGHFIVE

--- actions/database.php
+++ actions/database.php
@@ -1,10 +1,13 @@
-<?php 
+<?php
 
-try{
-$bd = new PDO ('mysql:host=localhost; dbname=forum; charset=utf8;', 'root', '');
-}
-catch(Exception $e){
-die('Une erreur a été trouvée: '. $e->getMessage());
+try {
+      $bd = new PDO(
+            'mysql:host=localhost; dbname=forum; charset=utf8;',
+            'root',
+            ''
+      );
+} catch (Exception $e) {
+      die('Une erreur a été trouvée: ' . $e->getMessage());
 }
 
- ?>
+?>
